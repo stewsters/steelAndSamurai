e406d2a0c80da137c2f17f55bda043efda53cb89
Adrian Moore <stewsters@gmail.com>
2017-03-05 00:14:36 -0600

    Initial commit from libGDX project setup tool

---
+++ desktop/src/com/steel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.steel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.steel.SteelSamuraiGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SteelSamuraiGame(), config);
+	}
+}
